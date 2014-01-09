39f8937e68fa8392ebe7b12860a0f158d0410721
Jakub Kaleta <jkaleta@gmail.com>
2014-01-09 16:13:15 +0100

    Generated example outputs

--- BattleshipGameboardGenerator/Program.cs
+++ BattleshipGameboardGenerator/Program.cs
@@ -19,10 +19,10 @@ namespace BattleshipGameboardGenerator
             //    5, 5, 6, 3, 2, 2, 2, 10, 7, 8, 40, 30, 30, 20, 20, 20, 10, 10, 10, 10, 5, 5, 6, 3, 2, 2, 
             //    20, 20, 10, 10, 10, 10, 5, 5, 6, 3, 2, 2, 2, 10, 7, 8, 2, 10, 7, 8 }, 50);
 
-            var strategy1 = new BruteForceBoardGenerationStrategy(GameConfiguration.MiltonBradleyConfiguration, 1000000);
+            //var strategy1 = new BruteForceBoardGenerationStrategy(GameConfiguration.MiltonBradleyConfiguration, 1000000);
             var strategy2 = new BruteForceBoardGenerationStrategy(GameConfiguration.RussianConfiguration, 1000000);
 
-            strategy1.GenerateBoards();
+            //strategy1.GenerateBoards();
             strategy2.GenerateBoards();
             Console.ReadLine();
         }
